3b5b537cc62bfafe85f213133c8f6859d247cfd3
kennyson017 <kennyson.florencio@pditabira.com>
2025-04-18 16:30:03 -0300

    Seletores

---
+++ Módulo 6 - Seletores/Seletores/index.js
@@ -0,0 +1,26 @@
+const main = () => {
+    
+// Mudar Titulo
+document.querySelector('h1').innerText = `Hello World`;
+
+// Mudar Estilo Lista
+const lista = document.getElementsByTagName('li')
+
+for (let i = 0; i < lista.length; i++) {
+    lista[i].style.color = 'blue';
+}
+
+// Mudar Paragrafo
+const paragraphs = document.querySelectorAll('p');
+
+paragraphs.forEach(paragraph => {
+    paragraph.classList = 'novaclasse';
+});
+
+// Muda Botão
+document.querySelector('button a').innerText = 'Novo Texto do Botão'
+
+document.querySelector('button').classList = 'botao'
+}
+
+setTimeout(main, 3000)
